9ff64e2010fe4c15aaf710fa3697f043475e1050
Andrea <ma.bayona@uniandes.edu.co>
2021-04-25 10:41:24 -0500

    add models and endpoints routes

---
+++ front/src/api/endpoints.ts
@@ -0,0 +1,9 @@
+export const SERVER_URL = "http://localhost:3000";
+
+export const UNCLASSIFIED_ANSWERS = "/unclassifiedAnswers";
+
+export const CLASSIFIED_ANSWERS = "/classifiedAnswers";
+
+export const GET_ANSWERS_BY_USER = "/getByUserName";
+
+export const UPDATE_QUESTION = "/updateQuestion";
---
+++ front/src/models/Answer.ts
@@ -0,0 +1,13 @@
+export type Answer = {
+    _id: string,
+    type: string,
+    user: string,
+    QID: number,
+    url_AcceptedAns: string,
+    ParentId: string,
+    AcceptedAnswerId: number
+    AScore: number,
+    ATags: string,
+    AID: number,
+    ABody: string,
+};
