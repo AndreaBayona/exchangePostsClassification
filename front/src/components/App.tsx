a098aae2d6542e302ca2c2ba7fe9eebded4ef626
Andrea <ma.bayona@uniandes.edu.co>
2021-04-24 19:26:28 -0500

    Create components

--- front/src/components/App.tsx
+++ front/src/components/App.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
-import '../css/App.css';
+import {Question} from "./Question";
+
+import {QData, QAns} from "./dataTemp";
+
+import {AppStyle, Header} from './AppStyles';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppStyle>
+      <Header>
+        ReactApp
+      </Header>
+      <Question question={QData} acceptedAnswer={QAns} classifierName="Andrea"/>
+    </AppStyle>
   );
 }
 
---
+++ front/src/components/Common/fonts.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Title = styled.h3<{inheritColor?: boolean}>`
+  text-align: start;
+  color: ${({inheritColor}) => !!inheritColor ? `inherit` : `black`};
+  margin: 0;
+`;
+
+export const Text = styled.p<{inheritColor?: boolean}>`
+  text-align: start;
+  color: ${({inheritColor}) => !!inheritColor ? `inherit` : `black`};
+  margin: 0;
+`;
