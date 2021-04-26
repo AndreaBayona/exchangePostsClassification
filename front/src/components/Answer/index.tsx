a5f879c2f095f31f7d64b738cc75f21421e3cfe2
Andrea <ma.bayona@uniandes.edu.co>
2021-04-26 00:32:41 -0500

    update form classif

--- front/src/components/Answer/index.tsx
+++ front/src/components/Answer/index.tsx
@@ -8,42 +8,51 @@ import {Answer as AnswerData} from "../../models/Answer";
 
 import {Container, Option, Options, Url} from "./styles";
 import {classificateAQuestion, ClassificationRequest} from "../../services";
+import {AlertWrapper, Wrapper} from "../AnswersPage/styles";
+import {Modal} from "react-bootstrap";
 
 type Props = {
     answer: AnswerData;
     type: string;
 };
 
-const Submit = (classification: Classification) => {
-
-    console.log("INPUTS", classification);
-    return true;
-};
-
 export const Answer: React.FunctionComponent<Props> = ({answer, type}) => {
 
     const [edit, setEdit ] = React.useState(false);
+    const [show, setShow] = React.useState(false);
+    const [msg, setMsg] = React.useState("");
 
-    React.useEffect(() => {
+    const Submit = (classification: Classification) => {
         const classificationRequest = {
-            AID: 50259726,
-            classification: {
-                typeOfLearning: "typeOfLearning2",
-                typeOfArchitecture: "typeOfArchitecture",
-                processingModel: "processingModel",
-                mlPipeline: "mlPipeline",
-                goodPractice: "goodPractice",
-                pitfall: "pitfall",
-                externalReferences: "externalReferences",
-                interesting: "interesting",
-            },
+            AID: answer.AID,
+            classification: classification,
         } as ClassificationRequest;
+        console.log("INPUTS", classification);
         classificateAQuestion(classificationRequest).then((ans) => {
             console.log(ans);
-        });
-    });
+            setShow(true);
+            setMsg("Your classification has been saved correctly!")
+        },
+            (ans) => {
+            return false;
+                setShow(true);
+                setMsg("Your form could not not be saved. Try again.")
+                console.log(ans);
+            }
+        );
+    };
 
     return (<Container>
+        {show &&
+        <AlertWrapper>
+            <Modal show={show} onHide={()=> setShow(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Form information</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>{msg}</Modal.Body>
+            </Modal>
+        </AlertWrapper>
+        }
         <Title>{type + ' ' + answer.AID}</Title>
         <Text>Score: {answer.AScore}</Text>
         <br/>
